8ea8521a2d5c6390dfef72da8a9c28252c8d6aa2
BH_Lin <binghuanlin@gmail.com>
2014-04-29 02:26:56 +0800

    Symptom:    modify layout for page "selfcheck" Bug ID: Root Cause: Solution: Note:

--- selfcheck.js
+++ selfcheck.js
@@ -15,13 +15,29 @@ function runTest() {
 	}
 
 
+	var portalFrame = document.getElementById("portalFrame")
 	$("#btnServer99").click(function() {
-		document.getElementById("portalFrame").src = "http://10.1.200.99/signin";
+		console.log("### >> ready to load url: http://10.1.200.99/signin");
+		$("#btnServerBeta").attr("class", 'btn btn-default');
+		$("#btnServer99").attr("class", 'btn btn-primary');
+		portalFrame.src = location.href.replace("selfcheck.html", "blank.html");
+		setTimeout(function(){
+			portalFrame.src = "http://10.1.200.99/signin";	
+		}, 1000)
 	});
 
 	$("#btnServerBeta").click(function() {
-		document.getElementById("portalFrame").src = "https://tmdphststest.trendmicro.com/signin";
+		console.log("### >> ready to load url: https://tmdphststest.trendmicro.com/signin");
+		$("#btnServer99").attr("class", 'btn btn-default');
+		$("#btnServerBeta").attr("class", 'btn btn-primary');
+		portalFrame.src = location.href.replace("selfcheck.html", "blank.html");
+		setTimeout(function(){
+			portalFrame.src = "https://tmdphststest.trendmicro.com/signin";
+		}, 1000)
+		
 	});
+
+	document.getElementById("timestamp").innerHTML = (new Date()).toString();
 }
 
 $("document").ready(runTest);
