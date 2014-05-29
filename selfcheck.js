ccae8a42b45a780667b097acbcbb6035618b236f
BH_Lin <binghuanlin@gmail.com>
2014-05-29 17:15:35 +0800

    remove iframe in selfcheck.html

--- selfcheck.js
+++ selfcheck.js
@@ -18,29 +18,6 @@ function runTest() {
 		document.getElementById("extFrame_fail").style.visibility = "hidden";
 	}
 
-
-	var portalFrame = document.getElementById("portalFrame")
-	$("#btnServer99").click(function() {
-		console.log("### >> ready to load url: http://10.1.201.174/signin");
-		$("#btnServerBeta").attr("class", 'btn btn-default');
-		$("#btnServer99").attr("class", 'btn btn-primary');
-		portalFrame.src = location.href.replace("selfcheck.html", "blank.html");
-		setTimeout(function(){
-			portalFrame.src = "http://10.1.201.174/signin";
-		}, 1000)
-	});
-
-	$("#btnServerBeta").click(function() {
-		console.log("### >> ready to load url: https://tmdphststest.trendmicro.com/signin");
-		$("#btnServer99").attr("class", 'btn btn-default');
-		$("#btnServerBeta").attr("class", 'btn btn-primary');
-		portalFrame.src = location.href.replace("selfcheck.html", "blank.html");
-		setTimeout(function(){
-			portalFrame.src = "https://tmdphststest.trendmicro.com/signin";
-		}, 1000)
-
-	});
-
 	document.getElementById("timestamp").innerHTML = (new Date()).toString();
 }
 
