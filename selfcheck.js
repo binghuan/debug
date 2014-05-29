7d9b50edc4a56a5c74fd9957be991f7ffd39dfa9
BH_Lin <binghuanlin@gmail.com>
2014-05-29 17:22:20 +0800

    update method to check extension frame existing.

--- selfcheck.js
+++ selfcheck.js
@@ -11,14 +11,22 @@ function runTest() {
 		document.getElementById("online_ok").style.visibility = "hidden";
 	}
 
-	// ### run test 2: check online or not.
-	if(document.getElementById("extensionFrame") == undefined) {
-		document.getElementById("extFrame_ok").style.visibility = "hidden";
-	} else {
-		document.getElementById("extFrame_fail").style.visibility = "hidden";
-	}
+	// ### run test 2: check extension frame exist or not.
+	setInterval(checkExtFrameExist, 1000);
 
 	document.getElementById("timestamp").innerHTML = (new Date()).toString();
+}
+
+
+function checkExtFrameExist() {
+
+	if(document.getElementById("extensionFrame") == undefined) {
+		document.getElementById("extFrame_ok").style.visibility = "hidden";
+		//console.log('is extension frame existed: no');
+	} else {
+		document.getElementById("extFrame_fail").style.visibility = "hidden";
+		//console.log('is extension frame existed: yes');
+	}
 }
 
 $("document").ready(runTest);
