142839a58d0ab476620cf6e8bdbaea5509ade83a
BH_Lin <binghuanlin@gmail.com>
2014-06-18 19:48:48 +0800

    Use jQuery to hide a DIV when the user clicks outside of it

--- testBindInput.js
+++ testBindInput.js
@@ -38,6 +38,17 @@ function windowPostMessage(elementWindow, messageOrObject, targetOrigin) {
 	}
 }
 
+$(document).mouseup(function (e)
+{
+    var container = $("#pageFrame");
+
+    if (!container.is(e.target) &&// if the target of the click isn't the container...
+        container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.hide();
+    }
+});
+
 $('document').ready(function() {
 
     $('#bindinput').click(function() {
