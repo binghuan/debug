d48ed3589d16d5c31daac2c7770882d9ff8b7171
BH_Lin <binghuanlin@gmail.com>
2014-06-27 17:51:09 +0800

    fix bug for query input field with type password

--- testBindInput.js
+++ testBindInput.js
@@ -58,51 +58,62 @@ $('document').ready(function() {
 
     $('#bindinput').click(function() {
         console.log("---> bind input field > start");
-        pwdField = document.getElementById('password');
-        pwdField.setAttribute("style", "background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3QsPDhss3LcOZQAAAU5JREFUOMvdkzFLA0EQhd/bO7iIYmklaCUopLAQA6KNaawt9BeIgnUwLHPJRchfEBR7CyGWgiDY2SlIQBT/gDaCoGDudiy8SLwkBiwz1c7y+GZ25i0wnFEqlSZFZKGdi8iiiOR7aU32QkR2c7ncPcljAARAkgckb8IwrGf1fg/oJ8lRAHkR2VDVmOQ8AKjqY1bMHgCGYXhFchnAg6omJGcBXEZRtNoXYK2dMsaMt1qtD9/3p40x5yS9tHICYF1Vn0mOxXH8Uq/Xb389wff9PQDbQRB0t/QNOiPZ1h4B2MoO0fxnYz8dOOcOVbWhqq8kJzzPa3RAXZIkawCenHMjJN/+GiIqlcoFgKKq3pEMAMwAuCa5VK1W3SAfbAIopum+cy5KzwXn3M5AI6XVYlVt1mq1U8/zTlS1CeC9j2+6o1wuz1lrVzpWXLDWTg3pz/0CQnd2Jos49xUAAAAASUVORK5CYII=); padding-right:18px; background-attachment:scroll; background-position:100% 50%;class=''; background-repeat: no-repeat;");
+        var inputFieldCount = $('input[type=password]').length;
+        if(inputFieldCount > 0) {
+            pwdField = $('input[type=password]')[0];
+        }
+
+        $(pwdField).css("background-image", "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAASCAYAAABSO15qAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3QsPDhss3LcOZQAAAU5JREFUOMvdkzFLA0EQhd/bO7iIYmklaCUopLAQA6KNaawt9BeIgnUwLHPJRchfEBR7CyGWgiDY2SlIQBT/gDaCoGDudiy8SLwkBiwz1c7y+GZ25i0wnFEqlSZFZKGdi8iiiOR7aU32QkR2c7ncPcljAARAkgckb8IwrGf1fg/oJ8lRAHkR2VDVmOQ8AKjqY1bMHgCGYXhFchnAg6omJGcBXEZRtNoXYK2dMsaMt1qtD9/3p40x5yS9tHICYF1Vn0mOxXH8Uq/Xb389wff9PQDbQRB0t/QNOiPZ1h4B2MoO0fxnYz8dOOcOVbWhqq8kJzzPa3RAXZIkawCenHMjJN/+GiIqlcoFgKKq3pEMAMwAuCa5VK1W3SAfbAIopum+cy5KzwXn3M5AI6XVYlVt1mq1U8/zTlS1CeC9j2+6o1wuz1lrVzpWXLDWTg3pz/0CQnd2Jos49xUAAAAASUVORK5CYII=)")
+            .css("padding-right", "18px")
+            .css("background-attachment", "scroll")
+            .css("background-position", "100% 50%")
+            .css("background-repeat", "no-repeat");
+
+
+        $(pwdField).on('mousemove', function(e) {
+            console.log(this.getBoundingClientRect());
+            var inputFieldPos = this.getBoundingClientRect();
+            if(isInTargetArea({x: e.offsetX, y: e.offsetY}, inputFieldPos) === true) {
+                $(this).css('cursor', 'pointer');
+            } else {
+                $(this).css('cursor', '');
+            }
+        });
+
+        $(pwdField).click(function(e) {
+            console.log(this.getBoundingClientRect());
+            var inputFieldPos = this.getBoundingClientRect();
+            if(isInTargetArea({x: e.offsetX, y: e.offsetY}, inputFieldPos) === true) {
+                    console.log('hit in area !!');
+
+                    if(document.getElementById('pageFrame') == undefined) {
+
+                        pageFrame = document.createElement('iframe');
+                        pageFrame.id = "pageFrame";
+                        pageFrame.height = "160px";
+                        pageFrame.width = inputFieldPos.width;
+                        pageFrame.src = 'testBindInputInner.html';
+                        pageFrame.setAttribute("frameborder",'2');
+                        pageFrame.setAttribute('style', 'z-index: 255;position: absolute; top:' + inputFieldPos.bottom + 'px;left:' + inputFieldPos.left+ 'px;');
+                        document.body.appendChild(pageFrame);
+                    } else {
+                        var isVisible = $('#pageFrame').is(":visible");
+                        if(isVisible === true) {
+                            console.log("ready to hide pageFrame");
+                            $('#pageFrame').hide();
+                        } else {
+                            console.log("ready to show pageFrame");
+                            $('#pageFrame').show();
+                        }
+
+                    }
+            }
+        });
 
         console.log("---> bind input field > done");
     });
 
-	$('#password').on('mousemove', function(e) {
-		console.log(this.getBoundingClientRect());
-        var inputFieldPos = this.getBoundingClientRect();
-        if(isInTargetArea({x: e.offsetX, y: e.offsetY}, inputFieldPos) === true) {
-            $(this).css('cursor', 'pointer');
-        } else {
-            $(this).css('cursor', '');
-        }
-	});
 
-    $('#password').click(function(e) {
-        console.log(this.getBoundingClientRect());
-        var inputFieldPos = this.getBoundingClientRect();
-        if(isInTargetArea({x: e.offsetX, y: e.offsetY}, inputFieldPos) === true) {
-                console.log('hit in area !!');
-
-                if(document.getElementById('pageFrame') == undefined) {
-
-                    pageFrame = document.createElement('iframe');
-                    pageFrame.id = "pageFrame";
-                    pageFrame.height = "160px";
-                    pageFrame.width = inputFieldPos.width;
-                    pageFrame.src = 'testBindInputInner.html';
-                    pageFrame.setAttribute("frameborder",'2');
-                    pageFrame.setAttribute('style', 'z-index: 255;position: absolute; top:' + inputFieldPos.bottom + 'px;left:' + inputFieldPos.left+ 'px;');
-                    document.body.appendChild(pageFrame);
-                } else {
-                    var isVisible = $('#pageFrame').is(":visible");
-                    if(isVisible === true) {
-                        console.log("ready to hide pageFrame");
-                        $('#pageFrame').hide();
-                    } else {
-                        console.log("ready to show pageFrame");
-                        $('#pageFrame').show();
-                    }
-
-                }
-        }
-    });
 
     if(window.addEventListener) {
         window.addEventListener("message",messageHandler);
@@ -129,10 +140,10 @@ function messageHandler(event) {
         //pwdField.value = eventData.strongPassword;
         var inputFieldCount = $('input[type=password]').length;
         if( inputFieldCount > 0) {
-        	var i;
-        	for(i =0; i< inputFieldCount; i++) {
-        		$('input[type=password]')[i].value = eventData.strongPassword;
-        	}
+            var i;
+            for(i =0; i< inputFieldCount; i++) {
+                $('input[type=password]')[i].value = eventData.strongPassword;
+            }
         }
 
         $('#pageFrame').hide();
