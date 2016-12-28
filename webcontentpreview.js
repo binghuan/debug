7a044ad8c95a9b057a858ec5ba29eb8098ac0afb
binghuan <binghuanlin@gmail.com>
2016-12-28 16:53:25 +0800

    Add Web page for html Content Preview

---
+++ webcontentpreview.js
@@ -0,0 +1,17 @@
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+//editor.getSession().setMode("ace/mode/javascript");
+
+$("#button_preview").click(function() {
+    //console.log("" + editor.getValue());
+
+    var frameWindow = document.getElementById('preview_frame').contentWindow;
+    frameWindow.postMessage(editor.getValue(), location.origin);
+
+    return;
+    $("#toNewWindow").html(editor.getValue())
+    var w = window.open();
+    var html = $("#toNewWindow").html();
+    $(w.document.body).html(html);
+
+});
