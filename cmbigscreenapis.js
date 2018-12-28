e6a713c0ede4507d9d6d1aa379090de0a274fb61
binghuan <binghuanlin@gmail.com>
2018-12-28 13:10:53 +0800

    add button for API test

--- cmbigscreenapis.js
+++ cmbigscreenapis.js
@@ -12,3 +12,8 @@ $("#button_to_get_device_id").click(function() {
     alert(androidAppProxy.getDeviceId());
 })
 
+$("#button_to_get_access_key").click(function() {
+    console.log("click: button_to_get_access_key");
+    alert(androidAppProxy.getAccessKey());
+})
+
