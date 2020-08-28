9b03a0df0d30126241313f57b03fa92960c3f288
BH_Lin <binghuanlin@gmail.com>
2020-08-28 11:10:22 +0800

    add button to convert parameters for CLI

--- convert4launch.js
+++ convert4launch.js
@@ -7,13 +7,10 @@ function replaceAll(str, find, replace) {
 let textfield = document.getElementById("textfield");
 let outputfield = document.getElementById("output");
 
-let btnConvert = document.getElementById("btn_convert");
+let btnConvert4launch = document.getElementById("btn_convert");
+let btnConvert2cli = document.getElementById("btn_convert2");
 
-
-
-btnConvert.onclick = () => {
-    console.log("onclick");
-
+function getArgs() {
     let args = [];
 
     // By lines
@@ -30,8 +27,26 @@ btnConvert.onclick = () => {
         args.push(line);
     }
 
+    return args;
+}
+
+btnConvert4launch.onclick = () => {
+    console.log("btnConvert4launch");
     console.log("### OUTPUT: ");
-    let result = "\"args\":" + JSON.stringify(args);
+    let result = "\"args\":" + JSON.stringify(getArgs());
+    console.log(result);
+    outputfield.value = result
+}
+
+btnConvert2cli.onclick = () => {
+    console.log("btnConvert2cli");
+    console.log("### OUTPUT: ");
+    let parameters = getArgs();
+    let result = "";
+    for (let i = 0; i < parameters.length; i++) {
+        result += ` '${parameters[i]}'`;
+    }
+
     console.log(result);
     outputfield.value = result
 }
